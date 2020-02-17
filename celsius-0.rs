5d0a8c3d31b822681d01e3e146ec527d8935e710
Tim McNamara <tim.mcnamara@canonical.com>
2020-02-17 17:09:21 +1300

    Clean up source code repo

--- celsius-0.rs
+++
@@ -1,6 +0,0 @@
-struct Celsius(f32);
-
-fn main() {
-  let nice_and_warm = Celsius(22.3);
-  println!("It's {}℃ today.", nice_and_warm);
-}
--- ch2/ch2-non-base2.rs
+++ ch2/ch2-non-base2.rs
@@ -3,8 +3,8 @@ fn main() {
   let thirty = 0o36; // <2>
   let three_hundred = 0x12C; // <3>
 
-  println!("base 10:  {}    {}       {}", three, thirty, three_hundred);
-  println!("base 2:  {:b} {:b} {:b}", three, thirty, three_hundred);
-  println!("base 8:   {:o}  {:o}       {:o}", three, thirty, three_hundred);
-  println!("base 16:  {:x}  {:x}       {:x}", three, thirty, three_hundred);
+  println!("{} {} {}", three, thirty, three_hundred);
+  println!("{:b} {:b} {:b}", three, thirty, three_hundred);
+  println!("{:o} {:o} {:o}", three, thirty, three_hundred);
+  println!("{:x} {:x} {:x}", three, thirty, three_hundred);
 }
