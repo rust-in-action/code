57d5989f0015f4cdd06ae3a1b90735a69c90d347
Bruno Chevalier <bmgg.chevalier@gmail.com>
2020-09-26 10:29:28 +0200

    Correct starting size of rows vectors (switch width and height, no impact on output)

--- ch2/ch2-mandelbrot/src/main.rs
+++ ch2/ch2-mandelbrot/src/main.rs
@@ -9,9 +9,9 @@ fn calculate_mandelbrot( // <2> This function converts between the output space 
   width: usize,          // <5> These two parameters represent the size of the output in "pixels"
   height: usize,         // <5>
 ) -> Vec<Vec<usize>> {
-  let mut all_rows: Vec<Vec<usize>> = Vec::with_capacity(width); // <6> Create a container to house the data from each row 
+  let mut all_rows: Vec<Vec<usize>> = Vec::with_capacity(height); // <6> Create a container to house the data from each row 
   for img_y in 0..height {                                          // <7> Iterating row by row allows us to print the output line by line
-    let mut row: Vec<usize> = Vec::with_capacity(height);
+    let mut row: Vec<usize> = Vec::with_capacity(width);
     for img_x in 0..width {
       let cx = x_min + (x_max - x_min) * (img_x as f64 / width as f64);  // <8> Calculate the proportion of the space we have covered in our output and covert that to points within the search space
       let cy = y_min + (y_max - y_min) * (img_y as f64 / height as f64); // <8>
